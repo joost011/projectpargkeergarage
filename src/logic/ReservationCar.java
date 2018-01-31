8f6335a85c5a4515368622f45a1946c16dd4c4e2
GreatGandalf <levi.staal15@gmail.com>
2018-01-31 22:22:53 +0100

    Reservation auto's betalen normaal tarief + 5 eenmalig

--- src/logic/ReservationCar.java
+++ src/logic/ReservationCar.java
@@ -10,7 +10,7 @@ public class ReservationCar extends Car {
     	Random random = new Random();
     	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 240);
         this.setMinutesLeft(stayMinutes);
-        this.setHasToPay(false);
+        this.setHasToPay(true);
         Double stayMinutesDouble = 1.0 * stayMinutes;
         this.setHasToPayAmount(((double) Math.round(stayMinutesDouble / 60 * Simulator.getRegularPricePerHour() *100) / 100) + 5);
     }
